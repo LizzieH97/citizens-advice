7d1c66c3bccf006f409549d431f0b2cf5a4e9142
LizzieH97 <erhughes97@gmail.com#>
2025-08-05 15:11:59 +0100

    styling the data, trying to split content string to make the title larger and convert string into working html

--- frontend/src/components/Content.tsx
+++ frontend/src/components/Content.tsx
@@ -1,14 +1,50 @@
-"use client";
+import parse from "html-react-parser";
+import { DataObj } from "../types/index";
 
-import { Source, DataObj } from "../types/index";
-
-type Props = { data: DataObj };
-
-export default function Content({ data }: Props) {
-  // TODO: Implement the content component to render out the content and sources
+function decodeHtml(html: string) {
+  const parser = new DOMParser();
   return (
-    <div className="border-2 border-gray-300 p-4 rounded-md">
-      Individual content and sources should go in this component
+    parser.parseFromString(html, "text/html").documentElement.textContent || ""
+  );
+}
+export default function Content({ data }: { data: DataObj }) {
+  const decoded = decodeHtml(data.content);
+
+  const cleanedContent = decoded.replaceAll("<b>", "<br/><b>");
+
+  const match = cleanedContent.match(/^([\s\S]*?):/);
+  const matchedTitle = match?.[0] || "";
+  const title = matchedTitle;
+  const body = cleanedContent.slice(matchedTitle.length);
+
+  return (
+    <div className="border-2 border-gray-300 p-4 bg-sky-700 rounded-md">
+      <h1 className="text-white text-2xl mb-2">{parse(title)}</h1>
+      <div className="text-base text-white">{parse(body)}</div>
+
+      {data.sources.map((source) => (
+        <section
+          key={source.id}
+          className="flex flex-row items-center justify-start p-4 m-2 bg-white border-2 border-sky-900 rounded-3xl text-black text-xl w-3/4"
+        >
+          {source.title}
+          <a
+            href={source.source}
+            className="relative w-16 h-16 block ml-4"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={source.favicon}
+              className="absolute inset-1 w-auto h-auto object-cover"
+              alt="favicon"
+            />
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 translate-x-[3px] -translate-y-1/2 text-xs text-white font-bold text-center">
+              CLICK HERE
+            </div>
+          </a>
+        </section>
+      ))}
     </div>
   );
 }
